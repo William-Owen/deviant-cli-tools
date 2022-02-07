a370ad268d29417dc56779c5037ad39508675c75
William Owen <git@wo.dev>
2022-02-07 12:28:59 +0000

    Provide better message formating

---
+++ cli.js
@@ -0,0 +1,7 @@
+// ./cli.js
+
+#!/usr/bin/env node
+
+let tools = require('./tools')
+
+tools()
--- tools.js
+++ tools.js
@@ -1,15 +1,62 @@
 const prompts = require('prompts');
 const moment = require('moment');
 
+// Menu
+
 (async () => {
 
-  const response = await prompts({
-    type: 'date',
-    name: 'date',
-    message: 'Please enter the event date.',
-    initial: new Date(),
+  const menuResponse = await prompts({
+    type: 'select',
+    name: 'option',
+    message: 'Choose a tool',
+    choices: [
+      { title: 'Discord Timestamp', description: 'Generate a quick discord timestamp', value: 'discordTimestamp' },
+      { title: 'Hello World', value: 'helloWorld', },
+    ],
+    initial: 0
   });
 
-  console.log(`\nThis event will start ${moment(response.date).format('dddd, MMMM D, YYYY [at] HH:mm')} (${moment(response.date).fromNow()})\n<t:${Math.floor(new Date(response.date).getTime() / 1000)}:F> (<t:${Math.floor(new Date(response.date).getTime() / 1000)}:R>)`);
+  switch (menuResponse.option) {
+
+    case "discordTimestamp": 
+
+      // Discord Timestamp
+
+      (async () => {
+
+        const response = await prompts({
+          type: 'date',
+          name: 'date',
+          message: 'Please enter the event date.',
+          initial: new Date(),
+        });
+
+        console.log(`\nThis event will start ${moment(response.date).format('dddd, MMMM D, YYYY [at] HH:mm')} (${moment(response.date).fromNow()})\n<t:${Math.floor(new Date(response.date).getTime() / 1000)}:F> (<t:${Math.floor(new Date(response.date).getTime() / 1000)}:R>) your time.`);
+
+      })();
+      
+      break;
+
+    case "helloWorld":
+
+      // Hello World
+
+      (async () => {
+
+        const response = await prompts({
+          type: 'confirm',
+          name: 'confirm',
+          message: 'Hello World',
+          initial: false
+        });
+
+        console.log(`Hello World`);
+
+      })();
+      
+      break;
+
+  }
 
 })();
+
